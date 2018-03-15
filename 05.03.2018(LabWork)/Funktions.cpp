7bb8d526b8f35961e230053b1d6bfa15092664d6
Клюкинк <@227_11>
2018-03-15 11:32:39 +0600

    k

--- 05.03.2018(LabWork)/Funktions.cpp
+++ 05.03.2018(LabWork)/Funktions.cpp
@@ -6,6 +6,63 @@
 
 using namespace std;
 
+static int cSymb(char*str, char symb)
+
+{
+
+	int count = 0;
+
+	for (int i = 0;i < strlen(str);i++)
+
+		if (*(str + i) == symb)count++;
+
+	return count;
+
+}
+
+
+
+
+void alf(char*str)
+
+{
+
+	unsigned char symb;
+
+	int k;
+
+	/*for (unsigned char i = 0;i <= 256;i++)
+
+	{
+
+	printf("%d - %c\n", (int)i, i);
+
+	}*/
+
+	for (int i = 224;i <= 255;i++)
+
+	{
+
+		k = cSymb(str, (unsigned char)i);
+
+		if (k != 0)
+
+		{
+
+			if (k > 1 && k < 5)
+
+				printf("Символ %c встречается %d раза\n", (unsigned char)i, k);
+
+			else
+
+				printf("Символ %c встречается %d раз\n", (unsigned char)i, k);
+
+		}
+
+	}
+
+}
+
 void change(char *str)
 {
 	char chs[] = ", ";
